96a15e2a7702be1e645557cf8e549ee333f12bf0
João Gabriel K. Zanella <joaogabrielkondazanella@gmail.com>
2022-12-11 21:13:18 -0300

    First commit

---
+++ src/ExemploLexer.java
@@ -0,0 +1,47 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+import java.io.IOException;
+
+public class ExemploLexer {
+
+    public static void imprimir_lexema(gramaticaLexer lexer, Token token) {
+        System.out.println("Lexema: " + token.getText());
+        System.out.println("    Classe: " + lexer.getVocabulary().getDisplayName(token.getType()));
+        System.out.println("    Linha:  " + token.getLine());
+    }
+
+    public static void imprimir_erro(Token token) {
+        System.out.println("\n==========================================");
+        System.out.println("Lexema invalido identificado!");
+        System.out.println("    Lexema: " + token.getText());
+        System.out.println("    Linha:  " + token.getLine());
+        System.out.println("    Inicio: " + token.getStartIndex());
+        System.out.println("    Fim:    " + token.getStopIndex());
+        System.out.println("==========================================\n");
+    }
+
+    public static void  main (String[] args){
+        String filename = "/home/joaogkz/IdeaProjects/projeto_parte1/codigo.txt";
+        try {
+            CharStream input = CharStreams.fromFileName(filename);
+            gramaticaLexer lexer = new gramaticaLexer(input);
+            Token token;
+            while (!lexer._hitEOF) {
+                token = lexer.nextToken();
+
+                if (lexer.getVocabulary().getDisplayName(token.getType()).equals("ERROR")) {
+                    imprimir_erro(token);
+                }
+                else {
+                    imprimir_lexema(lexer, token);
+                }
+            }
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/gramatica.g4
@@ -0,0 +1,33 @@
+grammar gramatica;
+
+fragment LETRA: [a-zA-Z];
+fragment DIGITO: [0-9];
+
+USE: 'use';
+TIPO: 'int' | 'float' | 'void';
+MAIN: 'main';
+DEF: 'def';
+IF: 'if';
+IFSE: 'ifse';
+ELSE: 'else';
+WHILE: 'while';
+RETURN: 'return';
+SEP_RE: '::';
+SEP_EX: ';';
+AC: '{';
+FC: '}';
+AP: '(';
+FP: ')';
+ID: LETRA(DIGITO|LETRA)*;
+ATR: '=';
+MAISMENOS: '+-';
+MAISMAIS: '++';
+MENOSMENOS: '--';
+OP_ARIT: '+' | '-' | '*' | '/' | '%';
+NUM: DIGITO+('.'DIGITO+)?;
+WS: [ \r\t\n]* ->skip;
+STR: '"' ('\\' ["\\] | ~["\\\r\n])* '"';
+ACOL : '[';
+FCOL : ']';
+OP_REL: ('<' | '<=' | '>=' | '>' | '=' | '!=');
+ERROR: . ;
